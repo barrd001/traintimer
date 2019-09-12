9b6a5d4f96cb568c02960248448d0ab30beffb5b
Devyn Barron <52061626+barrd001@users.noreply.github.com>
2019-09-11 23:46:12 -0700

    JS update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCik6HgCBTD5mq18kPLBQ5EOWxsSUGTJ78",
+    authDomain: "train-timer-cc9a7.firebaseapp.com",
+    databaseURL: "https://train-timer-cc9a7.firebaseio.com",
+    projectId: "train-timer-cc9a7",
+    storageBucket: "",
+    messagingSenderId: "886791668348",
+    appId: "1:886791668348:web:666a38b21da435f1af7ff9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 
 var name = '';
 var destination = '';
@@ -14,3 +27,4 @@ $('#add-train').on('click', function(event) {
 
     console.log(name, destination, startTime, freq);
 });
+
